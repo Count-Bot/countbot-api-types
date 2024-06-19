1f95920edb68f60dc219bc0416cded3b2c69ac26
Jaren Goldberg <fyrlex@gmail.com>
2024-06-19 10:37:32 -0400

    fix: params

--- src/rest/User.ts
+++ src/rest/User.ts
@@ -2,10 +2,17 @@ import { APICountingUser } from '../payloads/User.js';
 import { BaseAuthRouteOptions } from '../utils/base.js';
 
 export interface RESTGetAPIUserOptions extends BaseAuthRouteOptions<APICountingUser | null> {
-  Params: { userId: string; };
+  Params: {
+    userId: string;
+  };
+  Querystring: {
+    userId: string;
+  };
 }
 
 export interface RESTPostAPIUserOptions extends BaseAuthRouteOptions<APICountingUser | null> {
-  Body: { userId: string; };
+  Body: {
+    userId: string;
+  };
 }
 
