720c19f811637b4bce560d26ad90fa3d0a5d67ba
Jaren Goldberg <fyrlex@gmail.com>
2024-06-25 22:23:41 -0400

    fix: remove export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,4 @@ export * from './achievements.js';
 export * from './base.js';
 export * from './codes.js';
 export * from './items.js';
-export * from './languages.js';
 export * from './modifiers.js';
