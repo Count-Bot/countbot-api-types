6ab46eb99d5983563efa980c7fb6161966ddb476
Jaren Goldberg <fyrlex@gmail.com>
2025-05-22 19:20:22 -0400

    refactor: remove unused file

--- src/rest/Token.ts
+++
@@ -1,6 +0,0 @@
-import type { APIToken } from '../payloads/Token.js';
-import type { BaseAuthRouteOptions } from '../utils/base.js';
-
-export interface RESTPostAPIToken extends BaseAuthRouteOptions<APIToken | null> {
-  Body: { userId: string };
-}
